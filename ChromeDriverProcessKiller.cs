7392f811db07eeee2b82a0ba94fa421f2c522047
shlomograch <shlomegrach@gmail.com>
2018-07-20 10:48:11 -0600

    Add files via upload

---
+++ ChromeDriverProcessKiller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChromeDriverProcessKiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.GetProcesses()
+                .Where(x => x.ProcessName.ToLower()
+                    .StartsWith("chrome"))
+                .ToList()
+                .ForEach(x => x.Kill());
+        }
+    }
+}
